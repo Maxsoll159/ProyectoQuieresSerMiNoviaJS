c30aa74ec23cd980de367868360790418b4e60cc
Martin Rios <68676050+Maxsoll159@users.noreply.github.com>
2022-06-02 20:42:18 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,6 @@
 function norequiere(e){
     let target = e.target,
-    /*  SABER LA ALTURA DE LA PANTALLA PERO NO UTILICE
-        w = window.innerWidth,
-        h = window.innerHeight, 
-        */
+
      randomW = numeroRandom(0, 500),
      randomH = numeroRandom(0, 900);
      
